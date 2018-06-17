a68a1d54f02b48263bc07cd7fcd9d44d4437b429
Wei Siyi <weisiyi2@gmail.com>
2018-06-17 10:52:30 -0400

    upload the front

---
+++ LPworkfile/pages/wantsell/wantsell.js
@@ -0,0 +1,127 @@
+// pages/wantsell/wantsell.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    openid:'',
+    tempFilePaths: {},
+    bookname:'',
+    bookyear:'',
+    instructor:'',
+    description:'',
+    course_code:''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+
+  chooseimage: function () {
+    var that = this;
+    wx.showActionSheet({
+      itemList: ['从相册中选择', '拍照'],
+      itemColor: "#11110f",
+      success: function (res) {
+        if (!res.cancel) {
+          if (res.tapIndex == 0) {
+            that.chooseWxImage('album')
+          } else if (res.tapIndex == 1) {
+            that.chooseWxImage('camera')
+          }
+        }
+      }
+    })
+
+  },
+
+  chooseWxImage: function (type) {
+    var that = this;
+    wx.chooseImage({
+      count:6,
+      sizeType: ['original', 'compressed'],
+      sourceType: [type],
+      success: function (res) {
+        console.log(res);
+        that.setData({
+          tempFilePaths: res.tempFilePaths,
+        })
+      }
+    })
+  },
+
+  submit_in: function(e){
+    const that = this;
+    that.setData({
+      first_name: e.detail.value.first_name,
+      second_name: e.detail.value.second_name,
+      wechat_name: e.detail.value.wechat_name,
+      email: e.detail.value.email,
+      phone: e.detail.value.phone,
+      program: e.detail.value.program,
+      course_code:e.detail.value.course_code,
+      bookname: e.detail.value.bookname,
+      bookyear: e.detail.value.bookyear,
+      instructor: e.detail.value.instructor,
+      description: e.detail.value.description
+    })
+      //wx.navigateTo({url:'../success_submit/success'})
+  },
+
+  check:function(){
+    
+  }
+})
---
+++ LPworkfile/utils/util.js
@@ -0,0 +1,26 @@
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+module.exports = {
+  formatTime: formatTime
+}
+
+function getCurrentPageUrl() {
+  var pages = getCurrentPages()    //获取加载的页面
+  var currentPage = pages[pages.length - 1]    //获取当前页面的对象
+  var url = currentPage.route    //当前页面url
+  return url
+}
