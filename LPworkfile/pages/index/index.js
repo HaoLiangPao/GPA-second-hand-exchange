22fa7c4a4cc37b2c6fd354722f7a4611bcff5a03
Ken <mq.wang@mail.utoronto.ca>
2018-07-14 18:25:42 -0400

    fixed git conflict

--- LPworkfile/pages/index/index.js
+++ LPworkfile/pages/index/index.js
@@ -35,7 +35,6 @@ Page({
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
-      console.log("lalalallalalalala");
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
@@ -55,7 +54,6 @@ Page({
       })
     }
   },
-
   getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
@@ -70,14 +68,12 @@ Page({
       barcode: e.detail.value
     })
   },
-
   bindFocus: function (e) {
     this.setData({
       hiddenDropdown: false,
       hiddenClear: false
     })
   },
-
   bindcodeBlur: function (e) {
     this.setData({
       hiddenDropdown: true,
