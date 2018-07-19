fdc06d95354fca73266b9ba44683b97b5323e995
peilin.ye <peilin.ye@mail.utoronto.ca>
2018-07-19 11:20:22 -0400

    update shujia,type

--- LPworkfile/pages/ShuJia/shujia.js
+++ LPworkfile/pages/ShuJia/shujia.js
@@ -1,11 +1,21 @@
 // pages/ShuJia/shujia.js
-Page({
+var app = getApp()
+var my_book = ["Book1"]
+var arr_link = [1]
+var file = ""
 
+Page({
+  
   /**
    * 页面的初始数据
    */
   data: {
-  
+    items: [{
+      id: "1",
+      src: "../../image/pig.jpg",
+      text: my_book[0]
+    },],
+    url: file,
   },
 
   /**
@@ -71,8 +81,13 @@ Page({
   },
 
   history:function (){
+    wx.showToast({
+      title: '选择下架',
+      image: '/image/info.png',
+      duration: 2000
+    });
     wx.navigateTo({
-      url: '../history/history',
+      url: '../history/history'
     })
   }
 })
--- LPworkfile/pages/index/index.js
+++ LPworkfile/pages/index/index.js
@@ -107,23 +107,28 @@ Page({
   //照片查书(硬编码未完成)
   scan: function (e) {
     var that = this;
-    wx.scanCode({
-      success: function (res) {
-        that.setData({
-          barcode: res.result
-        });
-        that.query(e);
-      },
-      fail: function () {
-        that.setData({
-          barcode: "",
-          hiddenData: true
-        });
-      },
-      complete: function () {
+    wx.showToast({
+      title: '功能敬请期待',
+      image: '/image/pig.jpg',
+      duration: 2000
+    });
+    //wx.scanCode({
+    //  success: function (res) {
+    //    that.setData({
+    //      barcode: res.result
+    //    });
+    //    that.query(e);
+    //  },
+    //  fail: function () {
+    //    that.setData({
+    //      barcode: "",
+    //      hiddenData: true
+    //    });
+    //  },
+    //  complete: function () {
         // complete
-      }
-    })
+    //  }
+    //})
   },
 
   //搜索方程 （待接课程编码）
